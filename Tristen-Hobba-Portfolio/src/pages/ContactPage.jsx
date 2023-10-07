9ae70b8e5e4ad3207be4172be6a7ac597c71fe7a
Tristenh <tristenhobba1@gmail.com>
2023-10-07 14:05:36 +1100

    added form in ContactPage.jsx, styled with Chakra

--- Tristen-Hobba-Portfolio/src/pages/ContactPage.jsx
+++ Tristen-Hobba-Portfolio/src/pages/ContactPage.jsx
@@ -1,7 +1,28 @@
-export default function ContactPage(){
-    return(
-        <div>
-            <p>contact</p>
-        </div>
-    )
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Center,
+} from "@chakra-ui/react";
+
+export default function ContactPage() {
+  return (
+    <form>
+      <Center mt={"5rem"}  maxW="100vw">
+        <FormControl>
+          <FormLabel>Contact form:</FormLabel>
+          <FormLabel>Name:</FormLabel>
+          <Input type="text" />
+          <FormLabel>Email address:</FormLabel>
+          <Input type="email" />
+          <FormLabel>Message:</FormLabel>
+          <Input type="text" />
+          <Center>
+          <Button w={"20rem"} mt={"2rem"} colorScheme="green">Submit</Button>
+          </Center>
+        </FormControl>
+      </Center>
+    </form>
+  );
 }
