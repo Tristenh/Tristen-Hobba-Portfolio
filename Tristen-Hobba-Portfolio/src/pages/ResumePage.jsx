b488470a7464a7e4d798b4b03a09445ca4a1c98a
Tristenh <tristenhobba1@gmail.com>
2023-10-08 22:18:27 +1100

    addded resume.docx, added link to download my resume and listed my proficiencies in front end and back end in ResumePage.jsx

--- Tristen-Hobba-Portfolio/src/pages/ResumePage.jsx
+++ Tristen-Hobba-Portfolio/src/pages/ResumePage.jsx
@@ -1,7 +1,30 @@
-export default function ResumePage(){
-    return(
-        <div>
-            <p>resume</p>
-        </div>
-    )
+export default function ResumePage() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <a  href="./src/assets/Tristen Hobba Resume.docx" download>
+        Download my resume
+      </a>
+      <h2>Front end proficiencies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootsrap</li>
+      </ul>
+      <h2>Back-end proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
+    </div>
+  );
 }
